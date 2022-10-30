5d02e0b53f6b6526b5b49e8e502801aa948d2e39
emptylls <songjiellarry@gmail.com>
2022-10-30 14:49:23 -0700

    translation

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,35 @@
 #![feature(str_split_as_str)]
+#![feature(iterator_try_collect)]
 
 mod tga;
 mod mobj;
+mod define;
+mod matrix;
+
 use mobj::Module;
+use define::*;
+use matrix::*;
 
 use mem::swap;
 use std::error::Error;
 use std::{env, mem};
+use std::f64::consts::PI;
 use std::iter::zip;
 
 use rand::Rng;
 use image::{GenericImage, GenericImageView, ImageBuffer, Pixel, Primitive, Rgb, Rgba, RgbaImage};
-use nalgebra::Vector3;
+use image::codecs::hdr::Rgbe8Pixel;
+use nalgebra::{Isometry3, Matrix, Matrix1x2, Matrix2, Matrix2x1, Point3, Rotation, Rotation3, Scale3, Transform3, Unit, Vector2, Vector3};
 use once_cell::sync::Lazy;
 
-static CWD: Lazy<String> = Lazy::new(|| env::current_dir().unwrap().into_os_string().into_string().unwrap());
-
-const WID: u32 = 1000;
-const HIG: u32 = 1000;
-const DEP: u32 = 255;
-
-const BLACK: Rgba<u8> = Rgba([0, 0, 0, 0]);
-const WHITE: Rgba<u8> = Rgba([255, 255, 255, 255]);
-const RED: Rgba<u8> = Rgba([255, 0, 0, 255]);
-const GREEN: Rgba<u8> = Rgba([0, 255, 0, 255]);
-const BLUE: Rgba<u8> = Rgba([0, 0, 255, 255]);
-
-type V3i = Vector3<i32>;
-type V3u = Vector3<u32>;
-type V3f = Vector3<f32>;
-type V3d = Vector3<f64>;
-
-fn p3d(v: &Vector3<f64>) -> V3d {
-    Vector3::new(((v[0]+1.) * (WID-1) as f64/2.),
-                 ((v[1]+1.) * (HIG-1) as f64/2.),
-                 ((v[2]+1.) * (DEP-1) as f64/2.)
+fn p3d(v: &V4d) -> V3d {
+    V3d::new((v[0]+1.) * (WID-1) as f64/2.,
+             (v[1]+1.) * (HIG-1) as f64/2.,
+             (v[2]+1.) * (DEP-1) as f64/2.
     )
 }
 
-fn p3i(v: &Vector3<f64>) -> V3i {
+fn p3i(v: &V4d) -> V3i {
     Vector3::new(((v[0]+1.) * (WID-1) as f64/2.) as i32,
                  ((v[1]+1.) * (HIG-1) as f64/2.) as i32,
                  ((v[2]+1.) * (DEP-1) as f64/2.) as i32
@@ -56,7 +47,7 @@ fn draw_line<T: Pixel, U: GenericImage + GenericImageView<Pixel = T>>(p0: V3i, p
     }
 }
 
-fn triangle(v012: [V3i; 3], vt012: [V3i; 3], zbuf: &mut Vec<u8>, img: &mut ImageBuffer<Rgba<u8>, Vec<u8>>, ttv: &RgbaImage) {
+fn triangle(v012: [V3i; 3], vt012: [V3i; 3], zbuf: &mut [u8], img: &mut RgbaImage, ttv: &RgbaImage, rgba: Rgba<u8>) {
     let [mut v0, mut v1, mut v2] = v012;
     let [mut vt0, mut vt1, mut vt2] = vt012;
 
@@ -71,35 +62,38 @@ fn triangle(v012: [V3i; 3], vt012: [V3i; 3], zbuf: &mut Vec<u8>, img: &mut Image
     for i in 0..x02l {
 
         let x02 = (v2.x - v0.x) * i / x02l + v0.x;
-        let y02 = (v2.y - v0.y) * i / x02l + v0.y;
         let y11 = if i < x01l {v0.y+(v1.y-v0.y)*i/x01l} else {v1.y+(v2.y-v1.y)*(i-x01l)/x12l};
-        let ylen = y02 - y11;
+        let ylen = (v2.y - v0.y) * i / x02l + v0.y - y11;
         let yneg = (ylen > 0) as i32 - (ylen < 0) as i32;
 
         let vtx02 = (vt2.x - vt0.x) * i / x02l + vt0.x;
-        let vty02 = (vt2.y - vt0.y) * i / x02l + vt0.y;
         let vty11 = if i < x01l {vt0.y+(vt1.y-vt0.y)*i/x01l} else {vt1.y+(vt2.y-vt1.y)*(i-x01l)/x12l};
-        let vtylen = vty02 - vty11;
+        let vtylen = (vt2.y - vt0.y) * i / x02l + vt0.y - vty11;
         let vtyneg = (vtylen > 0) as i32 - (vtylen < 0) as i32;
 
         let y11l = ylen as i32 * yneg + 1;
         let vty11l = vtylen as i32 * vtyneg + 1;
         (0..y11l).for_each(|y|{
             let (vx, vy) = (x02 as u32, (y11+y*yneg).unsigned_abs());
-            if zbuf[(vx *HIG+ vy) as usize] < z {
+            if vx < WID && vy< HIG && zbuf[(vx *HIG+ vy) as usize] < z {
                 img.put_pixel(vx, vy, *ttv.get_pixel(vtx02 as u32, (vty11+(y*vty11l*vtyneg/y11l)).unsigned_abs()));
+                // img.put_pixel(vx, vy, rgba);
                 zbuf[(vx *HIG+ vy) as usize] = z;
             }
+            // else if vx < WID && vy< HIG {
+            //     img2.put_pixel(vx, vy, *ttv.get_pixel(vtx02 as u32, (vty11+(y*vty11l*vtyneg/y11l)).unsigned_abs()));
+            //     // img2.put_pixel(vx, vy, rgba);
+            // }
         })
     }
 }
 
 
-trait PixRevs {
+trait PixOps {
     fn revs(&mut self);
 }
 
-impl PixRevs for RgbaImage {
+impl PixOps for RgbaImage {
     fn revs(&mut self) {
         let w = self.width();
         let h = self.height();
@@ -117,26 +111,45 @@ impl PixRevs for RgbaImage {
 
 fn main() -> Result<(), Box<dyn Error>>{
 
+    let x = Mx4d::new_rotation(V3d::new(PI/4.,0.,0.));
+    let y = Mx4d::new_rotation(V3d::new(0.,PI/4.,0.));
+    let z = Mx4d::new_rotation(V3d::new(0.,0.,PI/4.));
+    let s = Mx4d::new_nonuniform_scaling(&V3d::new(0.6, 0.6 ,0.6));
+    let m = Isometry3::translation(1.6, 0., 0.).to_homogeneous();
+    let t = y * s * m;
+    // let t = Mx4d::identity();
+
     let md = Module::new(CWD.clone() + "/res/obj/african_head.obj").unwrap();
     let mut ttv = image::open(CWD.clone() + "/res/img/african_head_diffuse.png").unwrap().to_rgba8();
     let mut img = image::RgbaImage::new(WID, HIG);
+    let mut img2 = image::RgbaImage::new(WID, HIG);
     let mut zbuf: Vec<u8> = vec![0; (HIG * WID) as usize];
-    ttv.revs();
     let (ttvx, ttvy) = (ttv.width() as f64, ttv.height() as f64);
+    ttv.revs();
 
-    let light_dir = Vector3::new(0f64,0f64,-1f64);
+
+    let light_dir = V3d::new(0.,0.,-1.);
+    let transV = md.v.iter().map(|v| t * v).collect::<Vec<V4d>>();
     for f in md.f {
-        let (v0, v1, v2) = (&md.v[f[0]-1], &md.v[f[3]-1], &md.v[f[6]-1]);
-        let vt012 = [&md.vt[f[1]-1], &md.vt[f[4]-1], &md.vt[f[7]-1]].map(|x| Vector3::new((x.x*ttvx) as i32, (x.y*ttvy) as i32, 0));
-        let intensity = light_dir.dot(&(v0 - v2).cross(&(v1 - v2)).normalize().scale(-1.));
+        // let (v0, v1, v2) = (md.v[f[0]-1], md.v[f[3]-1], md.v[f[6]-1]);
+        let (v0, v1, v2) = (transV[f[0]-1], transV[f[3]-1], transV[f[6]-1]);
+        let vt012 = [&md.vt[f[1]-1], &md.vt[f[4]-1], &md.vt[f[7]-1]].map(|x| V3t::new((x.x*ttvx) as i32, (x.y*ttvy) as i32, 0));
+        let intensity = light_dir.dot(&as_v3d(v0 - v2).cross(&as_v3d(v1 - v2)).normalize().scale(-1.));
+        // let intensity = light_dir.dot(&V3d::new(1.,1.,1.,));
 
         let l = (intensity * 255f64) as u8;
         if intensity > 0f64 {
-            triangle([p3i(v0), p3i(v1), p3i(v2)], vt012, &mut zbuf, &mut img, &ttv);
+            // triangle([p3i(&(t *v0)), p3i(&(t *v1)), p3i(&(t *v2))], vt012, &mut zbuf, &mut img, &ttv, Rgba([l,l,l,255]));
+            triangle([p3i(&v0), p3i(&v1), p3i(&v2)], vt012, &mut zbuf, &mut img, &ttv, Rgba([l,l,l,255]));
         }
+        // else {
+        //     triangle([p3i(&v0), p3i(&v1), p3i(&v2)], vt012, &mut zbuf, &mut img2, &ttv, Rgba([l,l,l,255]));
+        // }
     }
 
     img.revs();
     img.save(CWD.clone() + "/res/img/a.png").unwrap();
+    img2.revs();
+    img2.save(CWD.clone() + "/res/img/a2.png").unwrap();
     Ok(())
 }
--- src/mobj.rs
+++ src/mobj.rs
@@ -4,11 +4,14 @@ use std::fs;
 use std::str::{FromStr, SplitWhitespace};
 use nalgebra::Vector3;
 
+use crate::define::*;
+use crate::matrix::*;
+
 pub struct Module {
-    pub v: Vec<Vector3<f64>>,
+    pub v: Vec<V4d>,
     pub f: Vec<Vec<usize>>,
-    pub vt: Vec<Vector3<f64>>,
-    pub vn: Vec<Vector3<f64>>
+    pub vt: Vec<V4d>,
+    pub vn: Vec<V4d>
 }
 
 fn pnext<T: Debug + FromStr>(p: Option<&str>) -> T where <T as FromStr>::Err: Debug  {
@@ -20,19 +23,19 @@ impl Module {
 
         let d = fs::read_to_string(fpath)?;
 
-        let mut v: Vec<Vector3<f64>> = Vec::with_capacity(1258);
+        let mut v: Vec<V4d> = Vec::with_capacity(1258);
         let mut f: Vec<Vec<usize>> = Vec::with_capacity(2492);
-        let mut vt: Vec<Vector3<f64>> = Vec::with_capacity(1339);
-        let mut vn: Vec<Vector3<f64>> = Vec::with_capacity(1258);
+        let mut vt: Vec<V4d> = Vec::with_capacity(1339);
+        let mut vn: Vec<V4d> = Vec::with_capacity(1258);
         for p in d.split("\n") {
             let mut p = p.split_whitespace();
             match p.next().unwrap_or(" ") {
                 "#" => {println!("{}", p.collect::<String>())}
-                "v" => {v.push(Vector3::new(pnext(p.next()), pnext(p.next()), pnext(p.next())));}
+                "v" => {v.push(V4d::new(pnext(p.next()), pnext(p.next()), pnext(p.next()), 1.));}
                 "f" => {f.push(p.flat_map(|x| x.split("/").map(|y| y.parse::<usize>().unwrap())).collect());}
                 // "f" => {f.push((p.next().unwrap().split("/").next().unwrap().parse::<usize>().unwrap()-1,p.next().unwrap().split("/").next().unwrap().parse::<usize>().unwrap()-1,p.next().unwrap().split("/").next().unwrap().parse::<usize>().unwrap()-1)); }
-                "vt" => {vt.push(Vector3::new(pnext(p.next()), pnext(p.next()), pnext(p.next())));}
-                "vn" => {vn.push(Vector3::new(pnext(p.next()), pnext(p.next()), pnext(p.next())));}
+                "vt" => {vt.push(V4d::new(pnext(p.next()), pnext(p.next()), pnext(p.next()), 1.));}
+                "vn" => {vn.push(V4d::new(pnext(p.next()), pnext(p.next()), pnext(p.next()), 1.));}
                 _ => {}
             };
         }
