38023e4da6bacd9ef29e12efe41aabb23165b925
emptylls <songjiellarry@gmail.com>
2022-10-30 14:52:09 -0700

    trans

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 // mod tga;
 // mod mobj;
+// mod operation;
---
+++ src/matrix.rs
@@ -0,0 +1,14 @@
+
+
+// pub struct V4d {
+//     pub x: f64,
+//     pub y: f64,
+//     pub z: f64,
+//     pub w: f64
+// }
+//
+// pub struct V3d {
+//     pub x: f64,
+//     pub y: f64,
+//     pub z: f64
+// }
